70af58385a9a522b3a04f454a14a1bbd2be4c83a
xuyifan <xiaoding94@hotmail.com>
2020-08-17 21:04:27 +0800

    1. 平衡二叉搜索树 旋转;

--- DataStructures/src/balancebinarytree/AVLTree.java
+++ DataStructures/src/balancebinarytree/AVLTree.java
@@ -1,14 +1,20 @@
 package balancebinarytree;
 
 /**
- * 平衡二叉树
+ * 平衡二叉搜索树
+ *  insert方法使用了递归方法
+ *      为什么？因为是平衡排序二叉树，所以每插入一个节点就要判断树是否平衡，而且还要找出旋转的中心节点；
+ *      而且有一个问题目前没有找到非递归的解决方法：
+ *          那就是插入一个新节点后，插入新节点的节点父节点（新节点的爷爷节点）所在子树并不一定是非平衡的；
+ *      目前，我能找到并理解的方法就是递归方法，所以无奈采用递归方法。
+ *
  */
 public class AVLTree {
 
     public AVLTreeNode root;
 
     /**
-     * 求当前节点的高度(左子树高度和右子树高度中的最大值)
+     * 获取节点的高度
      * @param node
      * @return
      */
@@ -21,71 +27,106 @@ public class AVLTree {
     }
 
     /**
-     * 求当前节点的左子树高度
-     * @param node
-     * @return
+     * 以当前节点为根节点的子树不平衡，左子树高度 < 右子树高度
+     * @param curNode 当前节点
      */
-    public int leftHeight(AVLTreeNode node) {
-        if (node != null) {
-            if (node.leftChild == null) {
-                return 0;
-            } else {
-                return height(node.leftChild);
-            }
-        } else {
-            return 0;
-        }
+    private void leftRotation(AVLTreeNode curNode) {
+        // 1. 新建一个节点作为新的左子节点，其value值为当前节点值
+        AVLTreeNode newNode = new AVLTreeNode(curNode.value);
+        // 2. 新节点的左子节点 = 当前节点的左子节点
+        newNode.leftChild = curNode.leftChild;
+        // 3. 新节点的右子节点 = 当前节点右子节点的左子节点
+        newNode.rightChild = curNode.rightChild.leftChild;
+        // 4. 当前节点的value = 当前节点的右子节点的value值
+        curNode.value = curNode.rightChild.value;
+        // 5. 当前节点的左子节点 = 新节点
+        curNode.leftChild = newNode;
+        // 6. 当前节点的右子节点 = 当前节点的右子节点的右子节点
+        curNode.rightChild = curNode.rightChild.rightChild;
     }
 
     /**
-     * 求当前节点的右子树高度
-     * @param node
-     * @return
+     * 以当前节点为根节点的子树不平衡， 左子树高度 > 右子树高度
+     * @param curNode 当前节点
      */
-    public int rightHeight(AVLTreeNode node) {
-        if (node != null) {
-            if (node.rightChild == null) {
-                return 0;
-            } else {
-                return height(node.rightChild);
-            }
-        } else {
-            return 0;
-        }
+    private void rightRotation(AVLTreeNode curNode) {
+        // 1. 新建一个节点，其value值等于当前节点
+        AVLTreeNode newNode = new AVLTreeNode(curNode.value);
+        // 2. 新节点的右子树 = 当前节点的右子树
+        newNode.rightChild = curNode.rightChild;
+        // 3. 新节点的左子树等于当前节点的左子树的右子树
+        newNode.leftChild =curNode.leftChild.rightChild;
+        // 4. 当前节点的value = 当前节点的左子节点的value值
+        curNode.value = curNode.leftChild.value;
+        // 5. 当前节点的右子节点 = 新节点
+        curNode.rightChild = newNode;
+        // 6. 当前节点的左子节点 = 当前节点的左子节点的左子节点
+        curNode.leftChild = curNode.leftChild.leftChild;
     }
 
     /**
-     * 添加方法, < 添加到左节点， >= 添加到右子节点
-     * 感觉应该有更好的方法。。回家看看
-     * @param node 插入节点
+     * 对外暴露的insert方法
+     * @param node 需要插入的节点
      */
     public void insert(AVLTreeNode node) {
-        if (node == null) {
+        if (root == null) {
+            root = node;
+        } else {
+            insert(root, node);
+        }
+    }
+
+    /**
+     * 添加新节点，向以curNode为根节点的子树添加newNode
+     * @param curNode 以curNode为根节点的子树
+     * @param newNode 需要添加的节点
+     * TODO 目前没有想到非递归的方法？应该是能改进的
+     */
+    private void insert(AVLTreeNode curNode, AVLTreeNode newNode) {
+        if (newNode == null) {
             return;
         }
 
-        if (root == null) {
-            root = node;
+        if (newNode.value < curNode.value) {
+            if (curNode.leftChild != null) {
+                insert(curNode.leftChild, newNode);
+            } else {
+                curNode.leftChild = newNode;
+            }
+        } else {
+            if (curNode.rightChild != null) {
+                insert(curNode.rightChild, newNode);
+            } else {
+                curNode.rightChild = newNode;
+            }
+        }
+
+         /*
+         每插入一个节点就判断以curNode为根节点的子树是否平衡
+          */
+        if (height(curNode.rightChild) - height(curNode.leftChild) > 1) {
+            if (height(curNode.rightChild.leftChild) > height(curNode.rightChild.rightChild)) {
+                // 当前节点的右子树的左子树高度 > 当前节点的右子树的右子树（RL型）
+                // 先对当前节点的右子树右旋
+                rightRotation(curNode.rightChild);
+                // 然后对当前节点左旋
+            }
+            // 以curNode为根节点的树：其右子树高度 - 左子树高度 > 1 左旋(RR型)
+            leftRotation(curNode);
+
+            // 树以平衡，无需继续向下执行
             return;
         }
-        // 从根节点扫描
-        AVLTreeNode current = root;
-        while (true) {
-            if (node.value < current.value) {
-                if (current.leftChild != null) {
-                    current = current.leftChild;
-                } else {
-                    current.leftChild = node;
-                    break;
-                }
-            } else {
-                if (current.rightChild != null) {
-                    current = current.rightChild;
-                } else {
-                    current.rightChild = node;
-                    break;
-                }
+
+        if (height(curNode.leftChild) - height(curNode.rightChild) > 1) {
+            if (height(curNode.leftChild.rightChild) > height(curNode.leftChild.leftChild)) {
+                // 当前节点的左子树的右子树高度 > 当前节点的左子树的左子树高度（LR型）
+                // 先对当前节点的左子节点进行左旋
+                leftRotation(curNode.leftChild);
+                // 然后对当前节点进行右旋
             }
+            // 以curNode为根节点的树：其左子树高度 - 右子树高度 > 1 右旋（LL型）
+            rightRotation(curNode);
         }
     }
 
--- DataStructures/src/balancebinarytree/AVLTreeDemo.java
+++ DataStructures/src/balancebinarytree/AVLTreeDemo.java
@@ -2,16 +2,37 @@ package balancebinarytree;
 
 public class AVLTreeDemo {
     public static void main(String[] args) {
-        int[] array = {7, 3, 10, 12, 5, 1, 9, 4, 13, 14};
         AVLTree avlTree = new AVLTree();
-        for (int i = 0; i < array.length; i++) {
-            AVLTreeNode node = new AVLTreeNode(array[i]);
-            avlTree.insert(node);
+
+        // 左旋测试数组
+       // int[] arr = {4, 3, 6, 5, 7, 8};
+
+        // 右旋测试数组
+        // int[] arr = {10, 12, 8, 9, 7, 6};
+
+        // 双旋测试数组(LR型)
+        // int[] arr = {10, 11, 7, 6, 8, 9};
+
+        // 双旋测试数组(RL型)
+        int[] arr = {2, 1, 6, 5, 7, 3};
+
+
+        for (int i = 0; i < arr.length; i++) {
+            AVLTreeNode avlNode = new AVLTreeNode(arr[i]);
+            avlTree.insert(avlNode);
         }
 
-        int height = avlTree.height(avlTree.root);
-        System.out.println(height);
-
+        // 遍历
         avlTree.list();
+        System.out.println("当前树根节点是：" + avlTree.root);
+        // 根节点高度
+        int rootHeight = avlTree.height(avlTree.root);
+        System.out.println("这棵树根节点的高度：" + rootHeight);
+        // 根节点左子树高度
+        int rootLeftHeight = avlTree.height(avlTree.root.leftChild);
+        System.out.println("这棵树根节点左子树的高度：" + rootLeftHeight);
+        // 根节点右子树高度
+        int rootRightHeight = avlTree.height(avlTree.root.rightChild);
+        System.out.println("这棵树根节点右子树的高度：" + rootRightHeight);
     }
 }
